7886cc1c20d40092821b1fffe7fd3e1f880bcbf6
Priyanka Datar <priyankadatar2013@gmail.com>
2019-12-10 21:00:06 -0500

    bert changes

---
+++ sqlnet/model/modules/net_utils.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+def run_lstm(lstm, inp, inp_len, hidden=None):
+    # Run the LSTM using packed sequence.
+    # This requires to first sort the input according to its length.
+    sort_perm = np.array(sorted(list(range(len(inp_len))),
+        key=lambda k:inp_len[k], reverse=True))
+    sort_inp_len = inp_len[sort_perm]
+    sort_perm_inv = np.argsort(sort_perm)
+    if inp.is_cuda:
+        sort_perm = torch.LongTensor(sort_perm).cuda()
+        sort_perm_inv = torch.LongTensor(sort_perm_inv).cuda()
+
+    lstm_inp = nn.utils.rnn.pack_padded_sequence(inp[sort_perm],
+            sort_inp_len, batch_first=True)
+    if hidden is None:
+        lstm_hidden = None
+    else:
+        lstm_hidden = (hidden[0][:, sort_perm], hidden[1][:, sort_perm])
+
+    sort_ret_s, sort_ret_h = lstm(lstm_inp, lstm_hidden)
+    ret_s = nn.utils.rnn.pad_packed_sequence(
+            sort_ret_s, batch_first=True)[0][sort_perm_inv]
+    ret_h = (sort_ret_h[0][:, sort_perm_inv], sort_ret_h[1][:, sort_perm_inv])
+    return ret_s, ret_h
+
+
+def col_name_encode(name_inp_var, name_len, col_len, enc_lstm):
+    #Encode the columns.
+    #The embedding of a column name is the last state of its LSTM output.
+    name_hidden, _ = run_lstm(enc_lstm, name_inp_var, name_len)
+    name_out = name_hidden[tuple(range(len(name_len))), name_len-1]
+    ret = torch.FloatTensor(
+            len(col_len), max(col_len), name_out.size()[1]).zero_()
+    if name_out.is_cuda:
+        ret = ret.cuda()
+
+    st = 0
+    for idx, cur_len in enumerate(col_len):
+        ret[idx, :cur_len] = name_out.data[st:st+cur_len]
+        st += cur_len
+    ret_var = Variable(ret)
+
+    return ret_var, col_len
+
+def att_flow_layer(c, q,softmax,att_weight_cq,att_weight_c,att_weight_q):
+        """
+        :param c: (batch, c_len, hidden_size * 2)
+        :param q: (batch, q_len, hidden_size * 2)
+        :return: (batch, c_len, q_len)
+        """
+        c_len = c.size(1)
+        q_len = q.size(1)
+
+        # (batch, c_len, q_len, hidden_size * 2)
+        #c_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1)
+        # (batch, c_len, q_len, hidden_size * 2)
+        #q_tiled = q.unsqueeze(1).expand(-1, c_len, -1, -1)
+        # (batch, c_len, q_len, hidden_size * 2)
+        #cq_tiled = c_tiled * q_tiled
+        #cq_tiled = c.unsqueeze(2).expand(-1, -1, q_len, -1) * q.unsqueeze(1).expand(-1, c_len, -1, -1)
+
+        cq = []
+        for i in range(q_len):
+
+            #(batch, 1, hidden_size * 2)
+            qi = q.select(1, i).unsqueeze(1)
+            #(batch, c_len, 1)
+            ci = att_weight_cq(c * qi).squeeze()
+            cq.append(ci)
+        # (batch, c_len, q_len)
+        cq = torch.stack(cq, dim=-1)
+
+        # (batch, c_len, q_len)
+        s = att_weight_c(c).expand(-1, -1, q_len) + \
+            att_weight_q(q).permute(0, 2, 1).expand(-1, c_len, -1) + \
+            cq
+
+        # (batch, c_len, q_len)
+        a = F.softmax(s, dim=2)
+        # (batch, c_len, q_len) * (batch, q_len, hidden_size * 2) -> (batch, c_len, hidden_size * 2)
+        c2q_att = torch.bmm(a, q)
+        # (batch, 1, c_len)
+        b = F.softmax(torch.max(s, dim=2)[0], dim=1).unsqueeze(1)
+        # (batch, 1, c_len) * (batch, c_len, hidden_size * 2) -> (batch, hidden_size * 2)
+        q2c_att = torch.bmm(b, c).squeeze()
+        # (batch, c_len, hidden_size * 2) (tiled)
+        q2c_att = q2c_att.unsqueeze(1).expand(-1, c_len, -1)
+        # q2c_att = torch.stack([q2c_att] * c_len, dim=1)
+
+        # (batch, c_len, hidden_size * 8)
+        x = torch.cat([c, c2q_att, c * c2q_att, c * q2c_att], dim=-1)
+
+        x=c + c2q_att+ c * c2q_att + c * q2c_att
+        y=softmax(x)
+        return y
+
+
